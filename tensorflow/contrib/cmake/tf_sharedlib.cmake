3d96b639f5213b006285b7c49b5191eb74908067
Jan Vrany <jan.vrany@fit.cvut.cz>
2017-01-11 11:55:05 +0000

    CMake: Added definitions to build TF shared livrary
    ...for use in external projects. It creates `libtensorflow.so`
    and/or `tensorflow.dll` which can be then used from other
    languages using FFI.

---
+++ tensorflow/contrib/cmake/tf_sharedlib.cmake
@@ -0,0 +1,19 @@
+########################################################
+# Tensorflow shared library (.so / .dll) for use in 
+# in external projects
+########################################################
+add_library(tf_sharedlib SHARED
+    $<TARGET_OBJECTS:tf_core_lib>
+    $<TARGET_OBJECTS:tf_core_cpu>
+    $<TARGET_OBJECTS:tf_core_framework>
+    $<TARGET_OBJECTS:tf_core_ops>
+    $<TARGET_OBJECTS:tf_core_direct_session>
+    $<$<BOOL:${tensorflow_ENABLE_GRPC_SUPPORT}>:$<TARGET_OBJECTS:tf_core_distributed_runtime>>
+    $<TARGET_OBJECTS:tf_core_kernels>
+    $<$<BOOL:${tensorflow_ENABLE_GPU}>:$<TARGET_OBJECTS:tf_stream_executor>>
+)
+target_link_libraries(tf_sharedlib
+    ${tf_core_gpu_kernels_lib}
+    ${tensorflow_EXTERNAL_LIBRARIES}
+    tf_protos_cc
+)
