21222a079c28c583792bd14a1ed30339e0022c0d
Jan Vrany <jan.vrany@fit.cvut.cz>
2017-02-11 00:52:33 +0000

    CMake: oops, compile and link `c_api.cc` when building shared library
    Also use `WINDOWS_EXPORT_ALL_SYMBOLS` property to force export of
    all symbols so the library is actually usable. Thanks Gera and Javi
    for pointing this out.
    
    Use of `WINDOWS_EXPORT_ALL_SYMBOL` is certainly un-Windowsish and
    it's temporary. The problem is that C API does not have Windows'
    `__declspec(export)` so nothing is exported buy default.
    
    Proper solution would be either modify `c_api.cc` or provide a
    `.def` file. I incline to do the former, but this only make
    sense if there's any chance of this being integrated back to
    upstream. Need to think of about it - meanwhile, let's live with
    exporting it all.

--- tensorflow/contrib/cmake/tf_sharedlib.cmake
+++ tensorflow/contrib/cmake/tf_sharedlib.cmake
@@ -2,20 +2,24 @@
 # Tensorflow shared library (.so / .dll) for use in 
 # in external projects
 ########################################################
-add_library(tf_sharedlib SHARED
-    $<TARGET_OBJECTS:tf_core_lib>
+set(tf_c_api_srcs
+    "${tensorflow_source_dir}/tensorflow/c/c_api.h" 
+    "${tensorflow_source_dir}/tensorflow/c/c_api.cc" 
+)
+
+add_library(tf_sharedlib SHARED   
+    ${tf_c_api_srcs} 
     $<TARGET_OBJECTS:tf_core_cpu>
     $<TARGET_OBJECTS:tf_core_framework>
+    $<TARGET_OBJECTS:tf_core_lib>
     $<TARGET_OBJECTS:tf_core_ops>
-    $<TARGET_OBJECTS:tf_core_direct_session>
-    $<$<BOOL:${tensorflow_ENABLE_GRPC_SUPPORT}>:$<TARGET_OBJECTS:tf_core_distributed_runtime>>
-    $<TARGET_OBJECTS:tf_core_kernels>
-    $<$<BOOL:${tensorflow_ENABLE_GPU}>:$<TARGET_OBJECTS:tf_stream_executor>>
+    $<TARGET_OBJECTS:tf_cc_framework>
+    $<TARGET_OBJECTS:tf_cc_ops>       
 )
-target_link_libraries(tf_sharedlib
-    ${tf_core_gpu_kernels_lib}
-    ${tensorflow_EXTERNAL_LIBRARIES}
-    tf_protos_cc
+target_link_libraries(tf_sharedlib   
+   ${tensorflow_EXTERNAL_LIBRARIES}             
+   tf_protos_cc                                   
 )
 
 set_target_properties(tf_sharedlib PROPERTIES OUTPUT_NAME "tensorflow")
+set_target_properties(tf_sharedlib PROPERTIES WINDOWS_EXPORT_ALL_SYMBOLS true)
